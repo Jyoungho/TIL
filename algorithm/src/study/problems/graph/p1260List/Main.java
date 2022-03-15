cabb414e140e671190a37263eb9142fef489b0a4
정영호 <youngh0910@gmail.com>
2022-03-15 19:29:06 +0900

    p1260 dfs bfs list

--- algorithm/src/study/problems/graph/p1260List/Main.java
+++ algorithm/src/study/problems/graph/p1260List/Main.java
@@ -1,77 +1,52 @@
-package study.problems.graph.p1260;
+package study.problems.graph.p1260List;
 
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
     static int N, M, V;
-    static int[][] adj;
+    static ArrayList<Integer>[] adj;
     static boolean[] visit;
 
     static void input() {
         N = scan.nextInt();
         M = scan.nextInt();
         V = scan.nextInt();
-        adj = new int[N + 1][N + 1];
+        adj = new ArrayList[N + 1];
+        for (int i = 1;i <= N; i++)
+            adj[i] = new ArrayList<>();
         for (int i = 0; i < M; i++) {
             int x = scan.nextInt(), y = scan.nextInt();
-            adj[x][y] = adj[y][x] = 1;
+            adj[x].add(y);
+            adj[y].add(x);
         }
+        for (int i = 1;i<=N;i++)
+            // 시간복잡도 O(ElogE)
+            Collections.sort(adj[i]);
     }
 
-//    // matrix (인접 행렬)
-//    static void dfs(int start) {
-//        // x 를 방문했다.
-//        visit[start] = true;
-//        sb.append(start).append(' ');
-//        for (int y = 1; y <= N; y++) {
-//            if (adj[start][y] == 0) continue;
-//            if (visit[y]) continue;
-//            dfs(y);
-//        }
-//    }
-
-    // list 인접리스트
-    static void dfs(int start) {
+    // x 를 갈 수 있다는 걸 알고 방문한 상태
+    static void dfs(int x) {
         // x 를 방문했다.
-        visit[start] = true;
-        sb.append(start).append(' ');
-        for (int y = 1; y <= N; y++) {
-            if (adj[start][y] == 0) continue;
-            if (visit[y]) continue;
+        visit[x] = true;
+        sb.append(x).append(' ');
+
+        // x 에서 갈 수 있는 곳들을 작은 번호부터 모두 방문한다.
+        for (int y: adj[x]){
+
+            // y 를 이미 갈 수 있다는 사실을 안다면, 굳이 갈 필요 없다.
+            if (visit[y])
+                continue;
+
+            // y에서 갈 수 있는 곳들도 확인 해보자
             dfs(y);
         }
     }
 
-//    // matrix (인접 행렬)
-//    static void bfs(int start) {
-//        Queue<Integer> que = new LinkedList<>();
-//
-//        // start는 방문 가능한 점이므로 que에 넣어준다.
-//        que.add(start);
-//        visit[start] = true;  // start를 갈 수 있다고 표시하기 (중요!!!)
-//
-//        while (!que.isEmpty()) {  // 더 확인할 점이 없다면 정지
-//            int x = que.poll();
-//
-//            sb.append(x).append(' ');
-//            for (int y = 1; y <= N; y++) {
-//                if (adj[x][y] == 0) continue;
-//                if (visit[y]) continue;  // x 에서 y 를 갈 수는 있지만, 이미 탐색한 점이면 무시
-//
-//                // y를 갈 수 있으니까 que에 추가하고, visit 처리 하기!
-//                que.add(y);
-//                visit[y] = true;
-//            }
-//        }
-//    }
-
-    // list (인접리스트)
+    // start 에서 시작해서 갈 수 있는 정점들을 모두 탐색하기
     static void bfs(int start) {
         Queue<Integer> que = new LinkedList<>();
 
@@ -83,8 +58,7 @@ public class Main {
             int x = que.poll();
 
             sb.append(x).append(' ');
-            for (int y = 1; y <= N; y++) {
-                if (adj[x][y] == 0) continue;
+            for (int y: adj[x]){
                 if (visit[y]) continue;  // x 에서 y 를 갈 수는 있지만, 이미 탐색한 점이면 무시
 
                 // y를 갈 수 있으니까 que에 추가하고, visit 처리 하기!
@@ -94,21 +68,20 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
-        //input
-        input();
-
-        pro();
-    }
-
     static void pro() {
         visit = new boolean[N + 1];
         dfs(V);
-        sb.append("\n");
+        sb.append('\n');
         for (int i = 1; i <= N; i++) visit[i] = false;
         bfs(V);
         System.out.println(sb);
     }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
 
     static class FastReader {
         BufferedReader br;
@@ -155,4 +128,4 @@ public class Main {
             return str;
         }
     }
-}
+    }
