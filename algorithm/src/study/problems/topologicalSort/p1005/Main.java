845f606d430f06500d8d8235285e22b6e7459242
정영호 <youngh0910@gmail.com>
2022-03-18 12:01:07 +0900

    p1005 ACM Craft

--- algorithm/src/study/problems/topologicalSort/p1005/Main.java
+++ algorithm/src/study/problems/topologicalSort/p1005/Main.java
@@ -1,8 +1,10 @@
 package study.problems.topologicalSort.p1005;
 
 import java.io.*;
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 public class Main {
     static FastReader scan = new FastReader();
@@ -14,16 +16,44 @@ public class Main {
 
     static void input() {
         // Testcase 가 존재하는 문제이므로 "배열 초기화"에 유의하자
-        /* TODO */
+        N = scan.nextInt();
+        M = scan.nextInt();
+        adj = new ArrayList[N + 1];
+        indeg = new int [N + 1];
+        T = new int [N + 1];
+        T_done = new int [N + 1];
+        for (int i = 1; i <= N; i++) {
+            adj[i] = new ArrayList<>();
+            T[i] = scan.nextInt();
+        }
+        for (int i = 0; i < M; i++) {
+            int x = scan.nextInt(), y = scan.nextInt();
+            adj[x].add(y);
+            indeg[y]++;
+        }
     }
 
     static void pro() {
         Deque<Integer> queue = new LinkedList<>();
         // 제일 앞에 "정렬될 수 있는" 정점 찾기
-        /* TODO */
+        for (int i = 1; i <= N; i++) {
+            if (indeg[i] == 0) {
+                queue.add(i);
+                T_done[i] = T[i];
+            }
+        }
 
         // 위상 정렬 순서대로 T_done 계산을 함께 해주기
-        /* TODO */
+        while (!queue.isEmpty()) {
+            int x = queue.poll();
+            for (int y : adj[x]) {
+                indeg[y]--;
+                if (indeg[y] == 0) queue.add(y);
+                T_done[y] = Math.max(T_done[y], T_done[x] + T[y]);
+            }
+        }
+        int W = scan.nextInt();
+        System.out.println(T_done[W]);
     }
 
     public static void main(String[] args) {
