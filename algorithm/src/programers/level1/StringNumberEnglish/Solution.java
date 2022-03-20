19c90f3ed4179fe818690500b33d6787a974f59e
정영호 <youngh0910@gmail.com>
2022-03-20 13:16:36 +0900

    refactor: level1 english to number

--- algorithm/src/programers/level1/StringNumberEnglish/Solution.java
+++ algorithm/src/programers/level1/StringNumberEnglish/Solution.java
@@ -2,77 +2,10 @@ package programers.level1.StringNumberEnglish;
 
 class Solution {
     public int solution(String s) {
-        int parseInt, englishLength;
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            StringBuilder sbTemp = new StringBuilder();
-            if (c - 'a' > 0) {
-                parseInt = 0;
-                englishLength = 0;
-                switch (s.charAt(i)) {
-                    // 0일 경우
-                    case 'z':
-                        sb.append('0');
-                        englishLength = 4;
-                        break;
-                    // 1일 경우
-                    case 'o':
-                        sb.append('1');
-                        englishLength = 3;
-                        break;
-                    // 2, 3 일 경우
-                    case 't':
-                        if(s.charAt(i+1)=='w'){
-                            sb.append('2');
-                            englishLength = 3;
-                            break;
-                        }
-                        else {
-                            sb.append('3');
-                            englishLength = 5;
-                            break;
-                        }
-                        // 4, 5 일 경우
-                    case 'f':
-                        if(s.charAt(i+1)=='o'){
-                            sb.append('4');
-                            englishLength = 4;
-                            break;
-                        }
-                        else{
-                            sb.append('5');
-                            englishLength = 4;
-                            break;
-                        }
-                        // 6, 7 일 경우
-                    case 's':
-                        if(s.charAt(i+1)=='i'){
-                            sb.append('6');
-                            englishLength = 3;
-                            break;
-                        }
-                        else{
-                            sb.append('7');
-                            englishLength = 5;
-                            break;
-                        }
-                        // 8 일 경우
-                    case 'e':
-                        sb.append('8');
-                        englishLength = 5;
-                        break;
-                    //9 일경우
-                    case 'n':
-                        sb.append('9');
-                        englishLength = 4;
-                        break;
-                }
-                i = i + englishLength - 1;
-            } else {
-                sb.append(c);
-            }
+        String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < strArr.length; i++) {
+            s = s.replaceAll(strArr[i], Integer.toString(i));
         }
-        return Integer.parseInt(sb.toString());
+        return Integer.parseInt(s);
     }
 }
