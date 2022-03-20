936ef182d031900b4f8c8b78791d832fd7ef32a2
정영호 <youngh0910@gmail.com>
2022-03-20 12:37:28 +0900

    refactor: reporter

--- algorithm/src/programers/level1/report/Solution.java
+++ algorithm/src/programers/level1/report/Solution.java
@@ -6,7 +6,7 @@ class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         // 초기화 작업
         HashMap<String, Set<String>> map = new HashMap<>();
-        HashMap<String, Integer> idMap = new HashMap<>();
+        Map<String, Integer> idMap = new LinkedHashMap<>();
         int[] answer = new int[id_list.length];
         for (String id : id_list){
             idMap.put(id, 0);
@@ -15,14 +15,9 @@ class Solution {
         // 신고되는 사람을 key로 HashMap 구성
         for (String reportCase: report) {
             String[] reportCaseList = reportCase.split(" ");
-            String reporter = reportCaseList[0];
-            Set<String> stringList = map.get(reportCaseList[1]);
-            if (stringList == null) {
-                stringList = new HashSet<>();
-            }
-            stringList.add(reporter);
+            Set<String> stringList = map.getOrDefault(reportCaseList[1], new HashSet<>());
+            stringList.add(reportCaseList[0]);
             map.put(reportCaseList[1], stringList);
-
         }
 
         // 알람을 받는 숫자를 idMap 에 저장
@@ -35,11 +30,6 @@ class Solution {
             }
         }
 
-        // 정답배열에 순서에 맞춰 입력
-        for (int i = 0; i < id_list.length; i++) {
-            String s = id_list[i];
-            answer[i] = idMap.get(s);
-        }
-        return answer;
+        return idMap.values().stream().mapToInt(Integer::intValue).toArray();
     }
 }
