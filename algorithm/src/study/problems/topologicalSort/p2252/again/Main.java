b5b7adfb92f5b7d5c1f5dfceb1c5fe8bf29c2907
정영호 <youngh0910@gmail.com>
2022-03-27 18:32:43 +0900

    p2252 위상정렬 again

---
+++ algorithm/src/study/problems/topologicalSort/p2252/again/Main.java
@@ -0,0 +1,100 @@
+package study.problems.topologicalSort.p2252.again;
+
+import javax.swing.*;
+import java.io.*;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M;
+    static int[] indeg;
+    static ArrayList<Integer>[] adj;
+
+    static void input() {
+        N = scan.nextInt();
+        M = scan.nextInt();
+        adj = new ArrayList[N + 1];
+        indeg = new int[N + 1];
+        for (int i = 1; i <= N; i++)
+            adj[i] = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            int x = scan.nextInt(), y = scan.nextInt();
+            adj[x].add(y);
+            indeg[y]++;
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static void pro() {
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++){
+            if (indeg[i] == 0)
+                queue.add(i);
+        }
+        while (!queue.isEmpty()) {
+            int x = queue.poll();
+            sb.append(x).append(' ');
+            for (int y : adj[x]) {
+                indeg[y]--;
+                if (indeg[y] == 0) queue.add(y);
+            }
+        }
+        System.out.println(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
