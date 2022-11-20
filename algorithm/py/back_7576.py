2e4207f6bd558144c1006b4d915337049c550ede
Albert <youngh0910@gmail.com>
2022-11-20 18:17:09 +0900

    modify boj_7576 python

--- algorithm/py/back_7576.py
+++ algorithm/py/back_7576.py
@@ -3,9 +3,10 @@ from collections import deque
 m, n = map(int, input().split())
 matrix = [list(map(int, input().split())) for _ in range(n)]
 
-queue = deque([])
-dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-res = 0
+queue = deque()
+dx, dy = [-1, 1 , 0, 0], [0, 0, -1 , 1]
+
+result = 0
 
 for i in range(n):
     for j in range(m):
@@ -17,18 +18,21 @@ def bfs():
         x, y = queue.popleft()
         
         for i in range(4):
-            nx, ny = dx[i] + x, dy[i] + y
-            if 0 <= nx < n and 0 <= ny < m and matrix[nx][ny] == 0:
+            nx, ny = x + dx[i], y + dy[i]
+            if 0<= nx < n and 0 <= ny <m and matrix[nx][ny] == 0:
                 matrix[nx][ny] = matrix[x][y] + 1
                 queue.append([nx, ny])
 
+# 탐색 시작
 bfs()
-for i in matrix:
-    for j in i:
-        if j == 0:
+
+for row in matrix:
+    for row_col in row:
+        if row_col == 0:
             print(-1)
             exit(0)
-    
-    res = max(res, max(i))
 
-print(res - 1)
+    # 가장 오래된 시간을 추출
+    result = max(result, max(row))
+
+print(result - 1)
