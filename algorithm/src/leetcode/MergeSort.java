50cf935c425b181d94b4bed57841c2407a045b64
Albert <youngh0910@gmail.com>
2024-02-03 12:40:22 +0900

    feat: merge sort

---
+++ algorithm/src/leetcode/MergeSort.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        MergeSort mergeSort = new MergeSort();
+        int[] nums1 = {1, 2, 3, 0, 0,0};
+        int[] nums2 = {2, 5, 6};
+        mergeSort.merge(nums1, 3, nums2, 3);
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int pMerge = m + n - 1;
+
+        while (p2 >= 0) {
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[pMerge--] = nums1[p1--];
+            } else {
+                nums1[pMerge--] = nums2[p2--];
+            }
+        }
+    }
+}
---
+++ algorithm/src/leetcode/MergeSort.kt
@@ -0,0 +1,18 @@
+package leetcode
+
+
+fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): IntArray {
+    var i = m + n - 1  // index for nums1 -> nums1.size - 1
+    var first = m - 1  // index for nums1
+    var second = n - 1 // index for nums2
+
+    while (second >= 0)
+        nums1[i--] = if (first >= 0 && nums2[second] <= nums1[first]) nums1[first--]
+        else nums2[second--]
+
+    return nums1;
+}
+
+fun main() {
+    println("[" + merge(intArrayOf(1, 2, 3, 0, 0, 0), 3, intArrayOf(2, 5, 6), 3).joinToString { it.toString() } + "]")
+}
