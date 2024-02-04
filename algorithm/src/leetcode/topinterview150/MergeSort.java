c3be1853e1958639f49c8f1aff843ce073ba1e2b
Albert <youngh0910@gmail.com>
2024-02-04 15:49:38 +0900

    refactor: leetcode 파일 구조 변경

--- algorithm/src/leetcode/topinterview150/MergeSort.java
+++ algorithm/src/leetcode/topinterview150/MergeSort.java
@@ -1,4 +1,4 @@
-package leetcode;
+package leetcode.topinterview150;
 
 import java.util.Arrays;
 
--- algorithm/src/leetcode/topinterview150/MergeSort.kt
+++ algorithm/src/leetcode/topinterview150/MergeSort.kt
@@ -1,4 +1,4 @@
-package leetcode
+package leetcode.topinterview150
 
 
 fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): IntArray {
